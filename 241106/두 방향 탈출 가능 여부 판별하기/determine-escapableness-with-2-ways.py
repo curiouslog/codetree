6657d785ca64416aa1dc39f41459b3e0aae814ba
curiouslog <han000122@naver.com>
2024-11-06 21:32:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5097ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 241106/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241106/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -7,17 +7,21 @@ mat = [
     for _ in range(n)
 ]
 
+visited = [[False] * m for _ in range(n)]
+
 def dfs(x, y):
     global result
+
+    visited[y][x] == True
 
     if x == m - 1 and y == n - 1:
         result = 1
         return
     
-    if y != n - 1 and mat[y + 1][x] == 1:
+    if y != n - 1 and mat[y + 1][x] == 1 and visited[y + 1][x] == False:
         dfs(x, y + 1)
 
-    if x != m - 1 and mat[y][x + 1] == 1:
+    if x != m - 1 and mat[y][x + 1] == 1 and visited[y][x + 1] == False:
         dfs(x + 1, y)
 
 dfs(0, 0)
